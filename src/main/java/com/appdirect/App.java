ceb022a153ef7031943b4bd5051f01eabebc28a1
JonnyLinton <jonathan.linton@appdirect.com>
2016-10-27 14:44:36 -0400

    initial commit

---
+++ src/main/java/com/appdirect/App.java
@@ -0,0 +1,13 @@
+package com.appdirect;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
