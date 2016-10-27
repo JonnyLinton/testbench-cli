26f3c017f96cfd9f9525b4352746e528e7c9d7b0
JonnyLinton <jonathan.linton@appdirect.com>
2016-10-27 15:33:57 -0400

    Sample Parsing, Fat Jar

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # testbench-cli
 TestBench used for System Integrators (SI)
+
+##How to Run:
+mvn package
+java -jar target/testbench-cli-1.0-SNAPSHOT-jar-with-dependencies.jar -helloworld 
--- src/main/java/com/appdirect/App.java
+++ src/main/java/com/appdirect/App.java
@@ -1,13 +1,26 @@
 package com.appdirect;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) throws ParseException {
+
+		Options options = new Options();
+		options.addOption("helloworld", false, "prints hello world");
+
+		CommandLineParser parser = new DefaultParser();
+
+		CommandLine cmd = parser.parse(options, args);
+
+		if (cmd.hasOption("helloworld")) {
+			System.out.println("Hello World!");
+		}
+	}
 }
