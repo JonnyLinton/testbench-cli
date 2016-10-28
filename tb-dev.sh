b21efa365865d5dd04a4c080b0ad71a8bb2a4035
Gabriel Rubenovitch <gabriel.rubenovitch@appdirect.com>
2016-10-28 15:41:31 -0400

    tb-dev.sh is now starting and stopping the sample marketplace. You need docker for it.

--- tb-dev.sh
+++ tb-dev.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 set -euo pipefail
-trap "echo 'error: Script failed: see failed command above'" ERR
+
+function stopAppmarket {
+    docker stop appdirect-testbench-sample-marketplace > /dev/null
+    docker rm appdirect-testbench-sample-marketplace > /dev/null
+}
+
+trap "stopAppmarket && echo 'error: Script failed: see failed command above'" ERR
 
 version=1.0-SNAPSHOT
 
+# download & run the sample marketplace
+docker run --name appdirect-testbench-sample-marketplace -p 8888:8888 -d docker.appdirectondemand.com/sample-marketplace/sample-marketplace:1.0-SNAPSHOT > /dev/null
+
 java -jar target/testbench-cli-${version}.jar "$@"
+
+stopAppmarket
